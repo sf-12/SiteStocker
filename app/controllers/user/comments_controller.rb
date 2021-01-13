9118f659638f781e473e660c80538a26c6117b12
sf-12 <s.fukasawa12@gmail.com>
2021-01-13 12:33:22 +0900

    [add] コメント削除機能を追加

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -9,7 +9,10 @@ class User::CommentsController < ApplicationController
     redirect_to tweet_path(tweet.id)
   end
 
-  def destroy; end
+  def destroy
+    Comment.find_by(id: params[:id], tweet_id: params[:tweet_id]).destroy
+    redirect_to tweet_path(params[:tweet_id])
+  end
 
   private
 
