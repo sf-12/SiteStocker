e5b745fefc461b11e00d57b4b40f7d558ad8c5a4
sf-12 <s.fukasawa12@gmail.com>
2021-01-16 11:35:54 +0900

    [add] ユーザー管理機能を追加

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -5,7 +5,7 @@ class User::HomesController < ApplicationController
     gon.linkpreview_key = ENV['LINK_PREVIEW_API_KEY']
     # 退会済みユーザーの投稿および非公開設定になっている投稿は除外する
     active_tweet = Tweet.where(user_id: User.where(is_active: true).ids, is_opened: true)
-    @tweets = active_tweet.page(params[:page]).reverse_order
+    @tweets = active_tweet.page(params[:page]).per(20).reverse_order
     gon.tweet_id_list = @tweets.ids
   end
 
