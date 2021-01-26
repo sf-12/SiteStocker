4047eba6bb7bb0cc8a3c46dc804055c7bce563a4
sf-12 <s.fukasawa12@gmail.com>
2021-01-26 14:28:36 +0900

    [add] いいね機能とコメント機能の非同期通信

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -2,16 +2,14 @@
 
 class User::LikesController < ApplicationController
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    like = current_user.likes.new(tweet_id: tweet.id)
+    @tweet = Tweet.find(params[:tweet_id])
+    like = current_user.likes.new(tweet_id: @tweet.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
-    tweet = Tweet.find(params[:tweet_id])
-    like = current_user.likes.find_by(tweet_id: tweet.id)
+    @tweet = Tweet.find(params[:tweet_id])
+    like = current_user.likes.find_by(tweet_id: @tweet.id)
     like.destroy
-    redirect_to request.referer
   end
 end
