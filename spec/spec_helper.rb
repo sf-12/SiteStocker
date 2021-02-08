df9a69c7921cda354aa53bcca67c414e6cee5699
sf-12 <s.fukasawa12@gmail.com>
2021-02-08 10:45:54 +0900

    [add] カバレッジ計測のためにsimplecovを導入

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,4 +95,8 @@ RSpec.configure do |config|
   #   as the one that triggered the failure.
   #   Kernel.srand config.seed
   # =end
+
+  # カバレッジを表示する
+  require 'simplecov'
+  SimpleCov.start 'rails'
 end
